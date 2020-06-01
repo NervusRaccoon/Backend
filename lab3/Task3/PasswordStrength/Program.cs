5e6d9c3fba4d93e1f215ca2a6b349c55184c8e50
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2020-06-01 04:36:39 +0300

    fix task3 in lab3

--- lab3/Task3/PasswordStrength/Program.cs
+++ lab3/Task3/PasswordStrength/Program.cs
@@ -1,108 +1,150 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace PasswordStrength
 {
-    public static class Program
+    public class Program
     {
-        public static int CountUppercaseLetter(string password)
-        {
-            int uppercase = 0;
-            for (int i = 0; i < password.Length; i++)
-            {
-                if (password[i] >= 'A' && password[i] <= 'Z')
-                {
-                    uppercase += 1;
-                }
-            }
-            return uppercase;
-        }
-
-        public static int CountLowercaseLetter(string password)
-        {
-            int lowercase = 0;
-            for (int i = 0; i < password.Length; i++)
-            {
-                if (password[i] >= 'a' && password[i] <= 'z')
-                {
-                    lowercase += 1;
-                }
-            }
-            return lowercase;
-        }
-
-        public static int CountDigit(string password)
-        {
-            int numbers = 0;
-            for (int i = 0; i < password.Length; i++)
-            {
-                if (password[i] >= '0' && password[i] <= '9')
-                {
-                    numbers += 1;
-                }
-            }
-            return numbers;
-        }
-        public static int CountDuplicates(string password)
-        {
-            int duplicates = 0;
-
-            Dictionary<char, int> charDictionary = new Dictionary<char, int>();
-
-            foreach (char ch in password)
-            {
-                if (charDictionary.ContainsKey(ch))
-                {
-                    charDictionary[ch]++;
-                }
-                else
-                {
-                    charDictionary.Add(ch, 1);
-                }
-            }
-            foreach (var item in charDictionary)
-            {
-                if (item.Value > 1)
-                {
-                    duplicates += item.Value;
-                }
-            }
-            return duplicates;
-        }
-        public static int CountStrength(string password)
-        {
-            int strength = 0;
-            int len = password.Length;
-            strength += len * 4;
-            strength += (4 * CountDigit(password));
-            if (CountUppercaseLetter(password) > 0)
-            {
-                strength += (len - CountUppercaseLetter(password)) * 2;
-            }
-            if (CountLowercaseLetter(password) > 0)
-            {
-                strength += (len - CountLowercaseLetter(password)) * 2;
-            }
-            if ((len == CountUppercaseLetter(password) + CountLowercaseLetter(password)) || (CountUppercaseLetter(password) + CountLowercaseLetter(password) == 0))
-            {
-                strength -= len;
-            }
-            strength -= CountDuplicates(password);
-            return strength;
-        }
-        public static int Main(string[] args)
+        public const string InvalidArgumentsCount = "Incorrect number of arguments";
+        public static string GetPassword(string[] args)
         {
             if (args.Length != 1)
             {
-                Console.WriteLine("Incorrect number of arguments");
-                return 1;
+                throw new Exception(InvalidArgumentsCount);
             }
-
-            int strength;
-            string password = args[0];
-            strength = CountStrength(password);
-            Console.WriteLine(strength);
+            return args[0];
+        }
+        public static bool CheckLowercase(char ch)
+        {
+            return ch >= 'a' && ch <= 'z';
+        }
+        public static bool CheckUppercase(char ch)
+        {
+            return ch >= 'A' && ch <= 'Z';
+        }
+        public static bool CheckDigit(char ch)
+        {
+            return ch >= '0' && ch <= '9';
+        }
+        public static bool CheckPassword(string password)
+        {
+            if (password == "")
+            {
+                Console.WriteLine("Identifier must not be an empty string");
+                return false;
+            }
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (!CheckLowercase(password[i]) && !CheckUppercase(password[i]) && !CheckDigit(password[i]))
+                {
+                    Console.WriteLine("Identifier must not contain extra characters");
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int CheckCopies(string str)
+        {
+            int copies = 0;
+            Dictionary<char, int> storage = new Dictionary<char, int>();
+            foreach (char ch in str)
+            {
+                if (storage.ContainsKey(ch))
+                {
+                    storage[ch]++;
+                }
+                else
+                {
+                    storage.Add(ch, 1);
+                }
+            }
+            foreach (var i in storage)
+            {
+                if (i.Value > 1)
+                {
+                    copies += i.Value;
+                }
+            }
+            return copies;
+        }
+        public static int InitialStrength(int length)
+        {
+            return 4 * length;
+        }
+        public static int StrengthCaseLetters(int length, int numberOfCase)
+        {
+            return numberOfCase != 0 ? (length - numberOfCase) * 2 : 0;
+        }
+        public static int StrengthDigits(int digits)
+        {
+            return digits != 0 ? (4 * digits) : 0;
+        }
+        public static int StrengthWithOnlyDigitsOrLetters(int length, int lowercase, int uppercase, int digits)
+        {
+            if ((lowercase == 0 && uppercase == 0) || digits == 0)
+            {
+                return length;
+            }
             return 0;
+        }
+        public static int StrengthCopies(string password)
+        {
+            int copies = CheckCopies(password);
+            return copies;
+        }
+        public static int CalculateStrength(string password, int digits, int lowercase, int uppercase)
+        {
+            int strength = 0;
+            int length = password.Length;
+
+            strength += InitialStrength(length);
+            strength += StrengthDigits(digits);
+            strength += StrengthCaseLetters(length, uppercase);
+            strength += StrengthCaseLetters(length, lowercase);
+            strength -= StrengthWithOnlyDigitsOrLetters(length, lowercase, uppercase, digits);
+            strength -= StrengthCopies(password);
+
+            return strength;
+        }
+        public static int GetPasswordStrength(string password)
+        {
+            int digits = 0;
+            int lowercase = 0;
+            int uppercase = 0;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (CheckDigit(password[i]))
+                {
+                    digits++;
+                }
+                if (CheckLowercase(password[i]))
+                {
+                    lowercase++;
+                }
+                if (CheckUppercase(password[i]))
+                {
+                    uppercase++;
+                }
+            }
+            int strength = CalculateStrength(password, digits, lowercase, uppercase);
+            return strength;
+        }
+        public static void main(string[] args)
+        {
+            try
+            {
+                string password = GetPassword(args);
+                if (CheckPassword(password))
+                {
+                    int passwordStrength = GetPasswordStrength(password);
+                    Console.WriteLine(passwordStrength);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType().Name}: " + InvalidArgumentsCount);
+            }
         }
     }
 }
