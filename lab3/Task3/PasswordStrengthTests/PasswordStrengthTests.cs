ecdbcd0cd31eb5c537486146440658406cee7e4f
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2020-06-01 04:38:20 +0300

    fix task 3 in lab 3

--- lab3/Task3/PasswordStrengthTests/PasswordStrengthTests.cs
+++ lab3/Task3/PasswordStrengthTests/PasswordStrengthTests.cs
@@ -3,273 +3,233 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PasswordStrengthTests
 {
     [TestClass]
-    public class CheckErrorsInPassword
+    public class GetPasswordTests
     {
         [TestMethod]
-        public void ManyArgsuments()
+        public void MoreThanOneArgument_ShouldThrowException()
         {
-            string[] arguments = new string[] { "password1", "password2" };
-            int res = PasswordStrength.Program.Main(arguments);
-
-            Assert.AreEqual(1, res);
+            string[] twoArgs = { "aa", "" };
+            try
+            {
+                PasswordStrength.Program.GetPassword(twoArgs);
+            }
+            catch (System.Exception e)
+            {
+                Equals(e.Message, PasswordStrength.Program.InvalidArgumentsCount);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown");
         }
-
         [TestMethod]
-        public void ZeroArguments()
+        public void WithNoArgument_ShouldThrowException()
         {
-            string[] arguments = new string[] { };
-            int res = PasswordStrength.Program.Main(arguments);
-
-            Assert.AreEqual(1, res);
+            string[] noArgs = { };
+            try
+            {
+                PasswordStrength.Program.GetPassword(noArgs);
+            }
+            catch (System.Exception e)
+            {
+                Equals(e.Message, PasswordStrength.Program.InvalidArgumentsCount);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown");
         }
     }
-
     [TestClass]
-    public class CheckCountDigit
+    public class CheckLetterAndDigit
     {
         [TestMethod]
-        public void NoDigit()
+        public void DigitWithNumber_ShouldReturnTrue()
         {
-            string password = "password";
+            char number = '1';
+            bool res = PasswordStrength.Program.CheckDigit(number);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void Lowercase_ShouldReturnTrue()
+        {
+            char letter = 'a';
+            bool res = PasswordStrength.Program.CheckLowercase(letter);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void Uppercase_ShouldReturnTrue()
+        {
+            char letter = 'A';
+            bool res = PasswordStrength.Program.CheckUppercase(letter);
+            Assert.AreEqual(true, res);
+        }
+    }
+    [TestClass]
+    public class CheckPasswordTests
+    {
+        [TestMethod]
+        public void EmptyPassword_ShouldReturnFalse()
+        {
+            string emptyString = "";
+            bool result = PasswordStrength.Program.CheckPassword(emptyString);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void ExtraSymbols_ShouldReturnFalse()
+        {
+            string wrongPassword = "ab#";
+            bool result = PasswordStrength.Program.CheckPassword(wrongPassword);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void RightPassword_ShouldReturnTrue()
+        {
+            string rightPassword = "Daria2020";
+            bool result = PasswordStrength.Program.CheckPassword(rightPassword);
+            Assert.AreEqual(true, result);
+        }
+    }
+    [TestClass]
+    public class CheckCopiesTests
+    {
+        [TestMethod]
+        public void HaveCopies_ShouldReturnNumberOfCopies()
+        {
+            string str = "aba";
+            int resultNumber = PasswordStrength.Program.CheckCopies(str);
+            int expectedNumber = 2;
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+        [TestMethod]
+        public void NoCopies_ShouldReturnZero()
+        {
+            string str = "abc";
+            int resultNumber = PasswordStrength.Program.CheckCopies(str);
+            int expectedNumber = 0;
+            Assert.AreEqual(expectedNumber, resultNumber);
+        }
+    }
+    [TestClass]
+    public class GetStrengthTests
+    {
+        [TestMethod]
+        public void InitialStrength()
+        {
+            int length = 4;
+            int resultStrength = PasswordStrength.Program.InitialStrength(length);
+            int expectedStrength = 16;
+            Assert.AreEqual(expectedStrength, resultStrength);
+        }
+        [TestMethod]
+        public void StrengthWithUpperLetters()
+        {
+            int length = 4;
+            int uppercase = 2;
+            int resultStrength = PasswordStrength.Program.StrengthCaseLetters(length, uppercase);
+            int expectedStrength = 4;
+            Assert.AreEqual(expectedStrength, resultStrength);
+        }
+        [TestMethod]
+        public void StrengthWithLowerLetters()
+        {
+            int length = 4;
+            int lowercase = 2;
+            int resultStrength = PasswordStrength.Program.StrengthCaseLetters(length, lowercase);
+            int expectedStrength = 4;
+            Assert.AreEqual(expectedStrength, resultStrength);
+        }
+        [TestMethod]
+        public void StrengthWithDigits()
+        {
+            int digits = 4;
+            int resultStrength = PasswordStrength.Program.InitialStrength(digits);
+            int expectedStrength = 16;
+            Assert.AreEqual(expectedStrength, resultStrength);
+        }
+        [TestMethod]
+        public void StrengthWithNoDigits()
+        {
+            int length = 4;
             int digits = 0;
-
-            int result = PasswordStrength.Program.CountDigit(password);
-
-            Assert.AreEqual(digits, result);
+            int uppercase = 2;
+            int lowercase = 2;
+            int resultStrength = PasswordStrength.Program.StrengthWithOnlyDigitsOrLetters(length, lowercase,
+                uppercase, digits);
+            int expectedStrength = 4;
+            Assert.AreEqual(expectedStrength, resultStrength);
         }
-
         [TestMethod]
-        public void ManyDigits()
+        public void StrengthWithCopies()
         {
-            string password = "password111";
-            int digits = 3;
-
-            int result = PasswordStrength.Program.CountDigit(password);
-
-            Assert.AreEqual(digits, result);
+            string password = "hello";
+            int resultStrength = PasswordStrength.Program.StrengthCopies(password);
+            int expectedResult = 2;
+            Assert.AreEqual(expectedResult, resultStrength);
         }
     }
-
     [TestClass]
-    public class CheckCountUppercaseLetter
+    public class GetPasswordStrengthTests
     {
         [TestMethod]
-        public void NoUppercaseLetter()
-        {
-            string password = "nouppercase";
-            int up = 0;
-            int result = PasswordStrength.Program.CountUppercaseLetter(password);
-
-            Assert.AreEqual(up, result);
-        }
-
-        [TestMethod]
-        public void ManyUppercaseLetter()
-        {
-            string password = "MANY";
-            int up = 4;
-            int result = PasswordStrength.Program.CountUppercaseLetter(password);
-
-            Assert.AreEqual(up, result);
-        }
-    }
-
-    [TestClass]
-    public class CheckCountLowercaseLetter
-    {
-        [TestMethod]
-        public void NoLowercaseLetter()
-        {
-            string password = "NOLOWERCASE";
-            int low = 0;
-            int result = PasswordStrength.Program.CountLowercaseLetter(password);
-
-            Assert.AreEqual(low, result);
-        }
-
-        [TestMethod]
-        public void ManyLowercaseLetter()
-        {
-            string password = "manylowercase";
-            int low = 13;
-            int result = PasswordStrength.Program.CountLowercaseLetter(password);
-
-            Assert.AreEqual(low, result);
-        }
-    }
-
-    [TestClass]
-    public class CheckDuplicatesTest
-    {
-        [TestMethod]
-        public void StringWithDuplicates()
-        {
-            string str = "aaaa";
-            int result = PasswordStrength.Program.CountDuplicates(str);
-            int expected = 4;
-            Assert.AreEqual(expected, result);
-        }
-        [TestMethod]
-        public void NoDuplicates()
-        {
-            string str = "NoDuplicates";
-            int result = PasswordStrength.Program.CountDuplicates(str);
-            int expected = 0;
-            Assert.AreEqual(expected, result);
-        }
-    }
-
-    [TestClass]
-    public class CheckCountStrength
-    {
-        [TestMethod]
-        public void EmptyPassword()
-        {
-            string password = "";
-            int expected = 0;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithUppercaseLetter()
-        {
-            string password = "ABCD";
-            int expected = 12;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithLowercaseLetter()
-        {
-            string password = "abcde";
-            int expected = 15;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithLowercaseAndUppercaseLetter()
-        {
-            string password = "abcDE";
-            int expected = 25;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithDigit()
+        public void PasswordWithOnlyNumbers()
         {
             string password = "123";
-            int expected = 21;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            int expectedStrength = 21;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithLowercaseLetterAndDigit()
+        public void PasswordWithOnlyLowercase()
         {
-            string password = "abc123";
-            int expected = 42;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "abcd";
+            int expectedStrength = 12;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithUppercaseLetterAndDigit()
+        public void PasswordWithOnlyUppercase()
         {
-            string password = "ABC1";
-            int expected = 22;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "ABCD";
+            int expectedStrength = 12;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithUppercaseAndLowercaseLetterAndDigit()
+        public void PasswordWithUppercase()
         {
-            string password = "Abc12";
-            int expected = 42;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "aBCd";
+            int expectedStrength = 20;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithDuplicatesNoDigitNoUppercaseLetter()
+        public void PasswordWithNumbersAndLowercase()
         {
-            string password = "aaa";
-            int expected = 6;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "ab12";
+            int expectedStrength = 28;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithDuplicatesNoDigitNoLowercaseLetter()
+        public void PasswordWithNumbersAndUppercase()
         {
-            string password = "BBB";
-            int expected = 6;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "AB12";
+            int expectedStrength = 28;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithDuplicatesWithDigitNoUppercaseLetter()
+        public void PasswordWithCopies()
         {
-            string password = "aa11";
-            int expected = 24;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "abcd1a";
+            int expectedStrength = 28;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
-
         [TestMethod]
-        public void PassWithDuplicatesWithDigitNoLowercaseLetter()
+        public void StrongPassword()
         {
-            string password = "BB22";
-            int expected = 24;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithDuplicatesAndDigitNoLetter()
-        {
-            string password = "111";
-            int expected = 18;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithDuplicatesAndUppercaseAndLowercaseLetterNoDigit()
-        {
-            string password = "AaAa";
-            int expected = 16;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void PassWithDuplicatesAndDigitAndUppercaseAndLowercaseLetter()
-        {
-            string password = "AAaa123";
-            int expected = 56;
-            int result = PasswordStrength.Program.CountStrength(password);
-
-            Assert.AreEqual(expected, result);
+            string password = "ab12NeO";
+            int expectedStrength = 54;
+            int strength = PasswordStrength.Program.GetPasswordStrength(password);
+            Assert.AreEqual(expectedStrength, strength);
         }
     }
 }
